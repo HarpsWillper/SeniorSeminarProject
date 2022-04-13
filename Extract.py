0083f01fe0d08af045d9317e48b4cf7222da7e80
HarpsWillper <54717536+HarpsWillper@users.noreply.github.com>
2022-04-13 17:31:24 -0400

    Worked on data frames

--- Extract.py
+++ Extract.py
@@ -1,3 +1,21 @@
 import pandas as pd
-data = pd.read_csv("Clan Donations and War Data - Donations.csv")
-print(data)
+import requests
+import configs
+
+headers = {
+    'authorization': configs.my_key,
+    'Accept':'application/json'
+}
+
+clan_list_url = 'https://api.clashofclans.com/v1/clans/%23JPLR0G2G/members?limit=10'
+
+def get_clan_members():
+    response = requests.get(clan_list_url, headers=headers)
+    json_response = response.json()['items']
+    
+    df = pd.json_normalize(json_response)
+
+    member_stats = df.loc[:,['name','role','expLevel','trophies','clanRank','donations','donationsReceived']]
+    
+
+get_clan_members()
